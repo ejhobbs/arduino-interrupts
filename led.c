e8dc04d81195b42b75cb64beaf61ade34d49bd35
Edward Hobbs <eh362@kent.ac.uk>
2018-03-24 17:59:27 +0000

    light up pin 11, if interrupt on 12 received

--- led.c
+++ led.c
@@ -1,12 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define RED 0x01
 #define REDAMBER 0x03
 #define GREEN 0x04
 #define AMBER 0x02
+#define WHITE 0x08
 int main (void) {
-  DDRB = 0x07;
+  DDRB = 0x0F;
+  PORTB |= (1<<PORTB4);
+  PCICR |= (1<<PCIE0);
+  PCMSK0 |= (1<<PCINT4); //enable interrupt listening on PB4 (arduino pin 12)
+
+  sei(); //enable interrupts
 
   while(1){
     PORTB = RED;
@@ -17,5 +24,11 @@ int main (void) {
     _delay_ms(500);
     PORTB = AMBER;
     _delay_ms(500);
+  }
+}
+
+ISR (PCINT0_vect){
+  if (PINB & (1 << PINB4)){ //check if PB4 high
+    PORTB = WHITE;
   }
 }
